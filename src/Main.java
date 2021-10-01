5bd693b72fb1ac5f65e12b6585c90575c88f39df
uvsq22103560 <mansouryacine37gmail.com>
2021-10-01 14:51:10 +0200

    ajout d'une classe main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
-}
+    public static void main (String[] args) {
+        System.out.println("hello world");
+
+
+
+        fraction f = new fraction (56,8);
+        System.out.println(f.toString());
+
+
+     }
+    }
+
