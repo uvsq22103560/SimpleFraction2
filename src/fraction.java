5f2708a5caab8aa98ce5edd734603ec9d2d57ebf
uvsq22103560 <mansouryacine37gmail.com>
2021-10-01 15:43:45 +0200

    ajout d'une classe main

--- src/fraction.java
+++ src/fraction.java
@@ -1,2 +1,16 @@
 public class fraction {
+    private int numerateur;
+    private int denomirateur;
+
+     fraction(int num, int denom) {
+        this.numerateur = num;
+        this.denomirateur = denom;
+    }
+
+    public String toString() {
+        String str = this.numerateur + " et " + this.denomirateur;
+        return str;
+     }
+
 }
+
